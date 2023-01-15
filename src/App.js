adfb6ee9edeeed973910c062a7b7cd5edbcb82a5
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-01-15 19:31:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      <h1 style={{background:"red", color:"black"}}>hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
